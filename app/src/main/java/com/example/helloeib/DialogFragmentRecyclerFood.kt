a0ff366d8795c464c65b11a50c97308326e5c291
CMichel <cmichel@indracompany.com>
2020-11-25 17:07:58 -0300

    dialog start

---
+++ app/src/main/java/com/example/helloeib/DialogFragmentRecyclerFood.kt
@@ -0,0 +1,11 @@
+package com.example.helloeib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DialogFragmentRecyclerFood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_fragment_recycler_food)
+    }
+}
